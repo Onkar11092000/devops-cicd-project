736a78fb70fb1d02b137db32d08bc3fbcf849ff6
Onkar Borhade <onkarborhade703@gmail.com>
2026-06-17 19:34:40 +0530

    Initial Spring Boot Application

---
+++ src/main/java/com/onkar/devops/DevopsCicdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onkar.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCicdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCicdProjectApplication.class, args);
+	}
+
+}
